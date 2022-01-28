3c96dc4b74b9ebcfdd789a988ff7d3e2760ff3be
dbestech <dylan@dbestinfo.com>
2022-01-28 14:13:59 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/dbestech/food_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dbestech.food_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color textColor = const Color(0xFFccc7c5);
+  static final Color mainColor = const Color(0xFF89dad0);
+  //static final Color mainColor = const Color(0xFFfa7552);
+  static final Color iconColor1 = const Color(0xFFffd28d);
+  static final Color iconColor2 = const Color(0xFFfcab88);
+  static final Color paraColor = const Color(0xFF8f837f);
+  static final Color buttonBackgroundColor= const Color(0xFFf7f6f4);
+  static final Color signColor = const Color(0xFFa9a29f);
+  static final Color titleColor = const Color(0xFF5c524f);
+  static final Color mainBlackColor = const Color(0xFF332d2b);
+  //static final Color yellowColor = const Color(0xFFfa7552);
+  static final Color yellowColor = const Color(0xFFffd379);
+
+}
